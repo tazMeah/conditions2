405adbec9a1a65022154b753030b6e5f318f4c77
Montaz Meah <montaz.meah@mrm.com>
2021-06-10 13:51:09 -0400

    Only comments. Instructions listed and pseudo code.

---
+++ scripts.js
@@ -0,0 +1,33 @@
+"use scrict";
+
+/* ------- INSTRUCTIONS ---------
+
+
+Task: Identify the longest of three names.
+
+Build Specifications:
+Start with three variables name1, name2, and name3 which hold three names.
+
+The code should output the longest of the three names. e.g., "Ada Lovelace has the longest name.". If there is a tie between any two, list both. e.g., “Charles and Brendan tie for the longest name.”. Or if all three names are the same length, output “All three names, Dorothy, Charles, and Brendan, are the same length.”
+
+Make sure to test your code with many different combinations of lengths. There are 7 possible outcomes.
+
+*/
+
+// Pseudo coded...
+
+// declare 3 variables for names
+
+// 1. if name1 is the longest
+
+// 2. if name2 is the longest
+
+// 3. if name3 is the longest
+
+// 4. if name1 and name2 tie for the longest
+
+// 5. if name1 and name3 tie for the longest
+
+// 6. if name2 and name3 tie for the longest
+
+// 7. else all names are equally long
